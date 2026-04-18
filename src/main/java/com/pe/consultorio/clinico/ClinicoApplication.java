65c55bb5ed8e014a54d0e659cb806de79668e50f
marco995669858 <marco995669858@gmail.com>
2026-04-18 01:37:07 -0500

    Subiendo proyecto backend consultorio clinico

---
+++ src/main/java/com/pe/consultorio/clinico/ClinicoApplication.java
@@ -0,0 +1,13 @@
+package com.pe.consultorio.clinico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/consultorio/clinico/ClinicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.consultorio.clinico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
